d0375788efd2e62949f5dd54436f5db96c65a1af
Sebastian Nyberg <seb.nyberg90@gmail.com>
2022-09-05 10:02:42 +0200

    Simpler, faster stack
    This commit re-writes the doubly-linked-list stack into a regular slice
    of items.
    
    It reduces the number of allocations significantly, but may increase the
    total allocation at certain points due to the behaviour of growslice.
    
    The existing benchmarks were pushing to the stack for the entire
    benchmark duration. With a 10x speed increase, this caused an OOM kill
    on my machine. So the benchmarks were re-written not to push
    indefinitely.
    
    The tests required accessing private fields of the stack struct so they
    were rewritten to be internals-agnostic.
    
    Results:
    
    name                  old time/op    new time/op    delta
    Stack/Stack-100-6       10.2µs ± 1%     0.7µs ± 3%  -93.15%  (p=0.000 n=9+9)
    Stack/Stack-1000-6       102µs ± 1%       6µs ± 8%  -94.11%  (p=0.000 n=10+10)
    Stack/Stack-10000-6     1.02ms ± 2%    0.07ms ± 6%  -93.55%  (p=0.000 n=10+10)
    Stack/Stack-100000-6    11.1ms ± 3%     0.9ms ±12%  -91.95%  (p=0.000 n=10+9)
    
    name                  old alloc/op   new alloc/op   delta
    Stack/Stack-100-6       3.28kB ± 0%    2.04kB ± 0%  -37.80%  (p=0.000 n=10+10)
    Stack/Stack-1000-6      32.1kB ± 0%    25.2kB ± 0%  -21.42%  (p=0.000 n=10+10)
    Stack/Stack-10000-6      320kB ± 0%     358kB ± 0%  +11.73%  (p=0.000 n=9+9)
    Stack/Stack-100000-6    3.20MB ± 0%    4.10MB ± 0%  +28.16%  (p=0.000 n=10+10)
    
    name                  old allocs/op  new allocs/op  delta
    Stack/Stack-100-6          102 ± 0%         8 ± 0%  -92.16%  (p=0.000 n=10+10)
    Stack/Stack-1000-6       1.00k ± 0%     0.01k ± 0%  -98.80%  (p=0.000 n=10+10)
    Stack/Stack-10000-6      10.0k ± 0%      0.0k ± 0%  -99.81%  (p=0.000 n=10+10)
    Stack/Stack-100000-6      100k ± 0%        0k ± 0%  -99.97%  (p=0.000 n=10+10)

--- stack.go
+++ stack.go
@@ -2,42 +2,49 @@ package lane
 
 // Stack implements a Last In First Out data structure.
 //
-// Built upon a Deque container, it focuses its API on the following core
-// functionalities: Push, Pop, Head, Size, Empty.
-//
 // Every operation's has a time complexity of *O(1)*.
-//
-// Every operations over an instantiated Stack are goroutine-safe.
 type Stack[T any] struct {
-	container *Deque[T]
+	items []T
 }
 
 // NewStack produces a new Stack instance.
 //
-// When providing initialization items, those will be inserted as-is: lower index being the head of the stack.
-func NewStack[T any](items ...T) (stack *Stack[T]) {
-	// FIXME: unwrap here instead of depending on Deque's for clarity
-	return &Stack[T]{
-		container: NewDeque(items...),
-	}
+// When providing initialization items, those will be inserted as-is: lower
+// index being the head of the stack.
+func NewStack[T any](items ...T) *Stack[T] {
+	var s Stack[T]
+	s.items = items
+	return &s
 }
 
 // Push adds on an item on the top of the Stack.
 func (s *Stack[T]) Push(item T) {
-	s.container.Prepend(item)
+	s.items = append(s.items, item)
 }
 
 // Pop removes and returns the item on the top of the Stack.
+//
+// If the stack is empty, a default-initialized item and false is returned.
 func (s *Stack[T]) Pop() (item T, ok bool) {
-	return s.container.Shift()
+	n := len(s.items)
+	if n == 0 {
+		return item, false
+	}
+	x := s.items[n-1]
+	s.items = s.items[:n-1]
+	return x, true
 }
 
 // Head returns the item on the top of the Stack.
-func (s *Stack[T]) Head() (item T, ok bool) {
-	return s.container.First()
+//
+// If the stack is empty, a default-initialized item and false is returned.
+func (s Stack[T]) Head() (item T, ok bool) {
+	n := len(s.items)
+	if n == 0 {
+		return item, false
+	}
+	return s.items[n-1], true
 }
 
 // Size returns the size of the Stack.
-func (s *Stack[T]) Size() uint {
-	return s.container.Size()
-}
+func (s Stack[T]) Size() int { return len(s.items) }
--- stack_test.go
+++ stack_test.go
@@ -1,239 +1,65 @@
 package lane
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestNewStack(t *testing.T) {
-	t.Parallel()
+var v int
+var ok bool
 
-	testCases := []struct {
-		desc                    string
-		items                   []int
-		wantContainerFirstOk    bool
-		wantContainerFirstValue int
-		wantContainerSize       uint
-	}{
-		{
-			desc:                 "NewStack initializes a Stack",
-			wantContainerFirstOk: false,
-			wantContainerSize:    0,
-		},
-		{
-			desc:                    "NewStack with initializer produces FIFO ordering",
-			items:                   []int{1, 2, 3},
-			wantContainerFirstOk:    true,
-			wantContainerFirstValue: 1,
-			wantContainerSize:       3,
-		},
-	}
-
-	for _, tC := range testCases {
-		tC := tC
-
-		t.Run(tC.desc, func(t *testing.T) {
-			t.Parallel()
-
-			gotStack := NewStack(tC.items...)
-			gotContainerFirstValue, gotContainerFirstOk := gotStack.container.First()
-			gotContainerSize := gotStack.container.Size()
-
-			assert.Equal(t, tC.wantContainerFirstOk, gotContainerFirstOk)
-			assert.Equal(t, tC.wantContainerFirstValue, gotContainerFirstValue)
-			assert.Equal(t, tC.wantContainerSize, gotContainerSize)
-		})
-	}
-}
-
-func TestStackPush(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		desc                    string
-		stack                   *Stack[int]
-		pushValue               int
-		wantContainerSize       uint
-		wantContainerFirst      bool
-		wantContainerFirstValue int
-	}{
-		{
-			desc:                    "Push to an empty Stack inserts value",
-			stack:                   NewStack[int](),
-			pushValue:               42,
-			wantContainerSize:       1,
-			wantContainerFirst:      true,
-			wantContainerFirstValue: 42,
-		},
-		{
-			desc:                    "Push inserts value at the head",
-			stack:                   NewStack([]int{41, 40}...),
-			pushValue:               42,
-			wantContainerSize:       3,
-			wantContainerFirst:      true,
-			wantContainerFirstValue: 42,
-		},
-	}
-
-	for _, tC := range testCases {
-		tC := tC
-
-		t.Run(tC.desc, func(t *testing.T) {
-			t.Parallel()
-
-			tC.stack.Push(tC.pushValue)
-			gotContainerLen := tC.stack.container.Size()
-			gotContainerFirstValue, gotContainerFirstOk := tC.stack.container.First()
-
-			assert.Equal(t, tC.wantContainerSize, gotContainerLen)
-			assert.Equal(t, tC.wantContainerFirst, gotContainerFirstOk)
-
-			if tC.wantContainerFirst {
-				assert.Equal(t, tC.wantContainerFirstValue, gotContainerFirstValue)
+func BenchmarkStack(b *testing.B) {
+	for _, sz := range []int{100, 1000, 10000, 100000} {
+		b.Run(fmt.Sprintf("Stack-%d", sz), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				stack := NewStack[int]()
+				for i := 0; i < sz; i++ {
+					stack.Push(i)
+				}
+				for i := 0; i < sz; i++ {
+					v, ok = stack.Head()
+					v, ok = stack.Pop()
+				}
 			}
 		})
 	}
 }
 
-func BenchmarkNewStack(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		NewStack[int]()
-	}
-}
-
-func TestStackPop(t *testing.T) {
+func TestStack(t *testing.T) {
 	t.Parallel()
 
-	testCases := []struct {
-		desc                    string
-		stack                   *Stack[int]
-		wantPopOk               bool
-		wantPopValue            int
-		wantContainerSize       uint
-		wantContainerFirst      bool
-		wantContainerFirstValue int
-	}{
-		{
-			desc:               "Pop on an empty Stack",
-			stack:              NewStack[int](),
-			wantPopOk:          false,
-			wantContainerSize:  0,
-			wantContainerFirst: false,
-		},
-		{
-			desc:                    "Pop removes and returns the head value",
-			stack:                   NewStack([]int{42, 41, 40}...),
-			wantPopOk:               true,
-			wantPopValue:            42,
-			wantContainerSize:       2,
-			wantContainerFirst:      true,
-			wantContainerFirstValue: 41,
-		},
+	// Helpers
+	requireTrue := func(t *testing.T, cond bool, fmt string, args ...interface{}) {
+		if !cond {
+			t.Fatalf(fmt, args...)
+		}
+	}
+	requireFalse := func(t *testing.T, cond bool, fmt string, args ...interface{}) {
+		requireTrue(t, !cond, fmt, args...)
 	}
 
-	for _, tC := range testCases {
-		tC := tC
+	stack := NewStack([]int{1, 2}...)
 
-		t.Run(tC.desc, func(t *testing.T) {
-			t.Parallel()
+	v, ok := stack.Head()
+	requireTrue(t, ok, "Head() must return ok when there are items")
+	requireTrue(t, v == 2, "Head() must return last item, expected: %q, got: %q", 2, v)
 
-			gotPopValue, gotPopOk := tC.stack.Pop()
-			gotContainerLen := tC.stack.container.Size()
-			gotContainerFirstValue, gotContainerFirstOk := tC.stack.container.First()
+	stack.Push(3)
+	v, ok = stack.Head()
+	requireTrue(t, ok, "Head() must return ok when there are items")
+	requireTrue(t, v == 3, "Head() must return last item, expected: %q, got: %q", 3, v)
 
-			assert.Equal(t, tC.wantPopOk, gotPopOk)
-			assert.Equal(t, tC.wantPopValue, gotPopValue)
-			assert.Equal(t, tC.wantContainerSize, gotContainerLen)
-			assert.Equal(t, tC.wantContainerFirst, gotContainerFirstOk)
+	x, ok := stack.Pop()
+	requireTrue(t, ok, "Head() must return ok when there are items")
+	requireTrue(t, x == 3, "Head() must return last item, expected: %q, got: %q", 3, v)
 
-			if tC.wantContainerFirst {
-				assert.Equal(t, tC.wantContainerFirstValue, gotContainerFirstValue)
-			}
-		})
-	}
-}
-
-func BenchmarkStackPop(b *testing.B) {
-	b.ReportAllocs()
-
-	stack := NewStack[int]()
-
-	for i := 0; i < b.N; i++ {
-		stack.Push(i)
-	}
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		stack.Pop()
-	}
-}
-
-func TestStackHead(t *testing.T) {
-	t.Parallel()
-
-	testCases := []struct {
-		desc                    string
-		stack                   *Stack[int]
-		wantHeadOk              bool
-		wantHeadValue           int
-		wantContainerSize       uint
-		wantContainerFirst      bool
-		wantContainerFirstValue int
-	}{
-		{
-			desc:               "Head on an empty Stack",
-			stack:              NewStack[int](),
-			wantHeadOk:         false,
-			wantContainerSize:  0,
-			wantContainerFirst: false,
-		},
-		{
-			desc:                    "Head returns the head value and leaves the Stack untouched",
-			stack:                   NewStack([]int{42, 41, 40}...),
-			wantHeadOk:              true,
-			wantHeadValue:           42,
-			wantContainerSize:       3,
-			wantContainerFirst:      true,
-			wantContainerFirstValue: 42,
-		},
-	}
-
-	for _, tC := range testCases {
-		tC := tC
-
-		t.Run(tC.desc, func(t *testing.T) {
-			t.Parallel()
-
-			gotHeadValue, gotHeadOk := tC.stack.Head()
-			gotContainerLen := tC.stack.container.Size()
-			gotContainerFirstValue, gotContainerFirstOk := tC.stack.container.First()
-
-			assert.Equal(t, tC.wantHeadOk, gotHeadOk)
-			assert.Equal(t, tC.wantHeadValue, gotHeadValue)
-			assert.Equal(t, tC.wantContainerSize, gotContainerLen)
-			assert.Equal(t, tC.wantContainerFirst, gotContainerFirstOk)
-
-			if tC.wantContainerFirst {
-				assert.Equal(t, tC.wantContainerFirstValue, gotContainerFirstValue)
-			}
-		})
-	}
-}
-
-func BenchmarkStackHead(b *testing.B) {
-	b.ReportAllocs()
-
-	stack := NewStack[int]()
-
-	for i := 0; i < b.N; i++ {
-		stack.Push(i)
-	}
-
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		stack.Head()
-	}
+	// Empty the stack then try out-of-bounds Pop()/Head()
+	stack.Pop()
+	stack.Pop()
+	x, ok = stack.Pop()
+	requireFalse(t, ok, "Pop() must return not ok when there are no items")
+	requireTrue(t, x == 0, "Pop() must return zero when there are no items")
+	x, ok = stack.Head()
+	requireFalse(t, ok, "Head() must return not ok when there are no items")
+	requireTrue(t, x == 0, "Head() must return zero when there are no items")
 }
